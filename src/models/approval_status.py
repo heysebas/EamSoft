484a96ede21cbdeea52b64f1648379dadb4e17de
JPablo67 <juanpa.995@gmail.com>
2024-11-21 18:43:14 -0500

    class updates

--- src/models/approval_status.py
+++ src/models/approval_status.py
@@ -1,9 +1,13 @@
 # models/approval_status.py
 
+# Import the Enum base class for creating enumerations
 from enum import Enum
 
-
+# Define the ApprovalStatus class
+# This is an enumeration to represent the status of an approval process.
+# It inherits from both `str` and `Enum` to allow the enumeration values to be treated as strings.
 class ApprovalStatus(str, Enum):
-    PENDIENTE = "Pendiente"
-    APROBADO = "Aprobado"
-    RECHAZADO = "Rechazado"
+    # Enumeration members with their corresponding string values.
+    PENDIENTE = "Pendiente"  # The request is pending review.
+    APROBADO = "Aprobado"    # The request has been approved.
+    RECHAZADO = "Rechazado"  # The request has been rejected.
